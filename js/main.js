29a0ec9fb2e879c17f6cb5e27562062f4dd42adf
misha576 <76790298+misha576@users.noreply.github.com>
2022-04-23 21:48:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+import uch from './uch.js'
+
+const uch = [
+new uch('td','td','td')
+
+]
+
+const $uchbody = document.getElementsDyId('uch-body')
+
+function newuchTR(uch) {
+ const $uchTR = document.createElement('tr'),
+       $nameTD = document.createElement('td'),
+       $surenameTD = document.createElement('td'),
+       $reasonTD = document.createElement('td')
+
+       $nameTD.textContent = uch.getuch()
+       $surenameTD.textContent = uch.getuch()
+       $reasonTD.textContent = uch.getuch()
+
+
+
+ $uchTR.append($nameTD)
+ $uchTR.append($surenameTD)
+ $uchTR.append($reasonTD)
+
+ return $uchTR;
+
+}
+
+function render (){
+  const uchCopy = [...uch]
+
+  for (const uch of uchCopy){
+  $uchbody.append(newuchTR(uch))
+  }
+  
+}
+render()
---
+++ js/uch.js
@@ -0,0 +1,16 @@
+export default class uch{
+
+constructor(name,surename,reason) {
+ this.name = name
+ this.surename = surename
+ this.reason = reason
+
+}
+	
+const uch = {
+  name: 'bvz',
+  surename: 'bvz',
+  reason: 'bvz',
+
+}
+}
